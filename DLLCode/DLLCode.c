50786a6dfb0c45f9d0c5690638b305e1f3da742e
Иван Смолин <ivan.smolin.78@mail.ru>
2022-11-12 18:10:53 +0300

    patch 1.1

---
+++ DLLCode/DLLCode.c
@@ -0,0 +1,35 @@
+﻿#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hlnstDLL, DWORD dwReason, LPVOID IpReserved) {
+
+	BOOL bAllWenrWell = TRUE;
+
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	if (bAllWenrWell)
+
+		return TRUE;
+
+	else
+
+		return FALSE;
+
+}
+
+extern "C" __declspec(dllimport) int MyFunc(LPWSTR str);
+
+int MyFunc(LPWSTR str) {
+
+	MessageBox(NULL, str, L"Nise", MB_OK);
+	return 0;
+
+}
